a78db0c17b2e82194898411cfff149ba5147e750
jjwon03303 <senseme538@gmail.com>
2024-04-24 14:40:22 +0900

    초기 상태 커밋

---
+++ src/main/kotlin/com/example/springLottoJunggan/SpringLottoJungganApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springLottoJunggan
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringLottoJungganApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringLottoJungganApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/springLottoJunggan/SpringLottoJungganApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springLottoJunggan
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringLottoJungganApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
