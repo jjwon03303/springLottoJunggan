58ce8c052ceba6c2a0a22554c3894c235aa3fec9
jjwon03303 <senseme538@gmail.com>
2024-04-24 15:16:19 +0900

    로또 DTO 구현

---
+++ src/main/kotlin/com/example/springLottoJunggan/main/api/model/dto/lottoDTO.kt
@@ -0,0 +1,22 @@
+package com.example.springLottoJunggan.main.api.model.dto
+
+data class lottoDTO (
+    val n1 : Int,
+    val n2 : Int,
+    val n3 : Int,
+    val n4 : Int,
+    val n5 : Int,
+    val n6 : Int,
+    val n7b : Int,
+)
+
+
+data class lottoDTOReqeust(
+    val n1 : Int,
+    val n2 : Int,
+    val n3 : Int,
+    val n4 : Int,
+    val n5 : Int,
+    val n6 : Int,
+    val n7b : Int,
+)
